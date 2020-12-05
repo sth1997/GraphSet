31e07794c37adf8f50e0e51e03ecc6bcdf9fd633
sth1997 <765215342@qq.com>
2020-12-05 15:00:43 +0800

    Add do_intersection_128, but its performance is poor.

--- gpu/gpu_graph.cu
+++ gpu/gpu_graph.cu
@@ -227,6 +227,121 @@ __device__ unsigned int dev_cur_edge = 0;
  * @note：a和b并不是地位相等的。如果要进行in-place操作，请把输入放在a而不是b。
  * @todo：shared memory缓存优化
  */
+constexpr int MAX_INTERSECTION_CONCURRENCY = 128; // 一个warp一次做128个a数组中的元素（4*32），减少求前缀和的时间
+//目前性能较差（个人认为是shared memory读写次数较多），暂时别用这个函数，之后优化
+__device__ uint32_t do_intersection_128(uint32_t* out, const uint32_t* a, const uint32_t* b, uint32_t na, uint32_t nb)
+{
+    __shared__ uint32_t block_out_offset[MAX_INTERSECTION_CONCURRENCY * WARPS_PER_BLOCK];
+    __shared__ uint32_t block_tmp_prefix_sum[THREADS_PER_WARP * WARPS_PER_BLOCK];
+    __shared__ uint32_t block_out_size[WARPS_PER_BLOCK];
+
+    int wid = threadIdx.x / THREADS_PER_WARP; // warp id
+    int lid = threadIdx.x % THREADS_PER_WARP; // lane id
+    //uint32_t *out_offset = block_out_offset + wid * THREADS_PER_WARP;
+    uint32_t *out_offset = block_out_offset + wid * MAX_INTERSECTION_CONCURRENCY;
+    uint32_t* tmp_prefix_sum = block_tmp_prefix_sum + wid * THREADS_PER_WARP;
+    uint32_t &out_size = block_out_size[wid];
+
+    if (lid == 0)
+        out_size = 0;
+    int concurrency = THREADS_PER_WARP;
+    int shift = 0;
+    if (na > THREADS_PER_WARP)
+    {
+        if (na <= 64)
+        {
+            concurrency = 64;
+            shift = 1;
+        }
+        else
+        {
+            concurrency = MAX_INTERSECTION_CONCURRENCY;
+            shift = 2;
+        }
+    }
+
+    uint32_t v, num_done = 0;
+    while (num_done < na) {
+        for (int start_offset = lid; start_offset < concurrency; start_offset += THREADS_PER_WARP)
+        {
+            bool found = 0;
+            if (start_offset + num_done < na) {
+                uint32_t u = a[start_offset + num_done]; // u: an element in set a
+                /*
+                // 我这里这样写并没有变快，反而明显慢了
+                int x, s[3], &l = s[0], &r = s[1], &mid = s[2];
+                l = 0, r = int(nb) - 1, mid = (int(nb) - 1) >> 1;
+                while (l <= r && !found) {
+                    uint32_t v = b[mid];
+                    found = (v == u);
+                    x = (v < u);
+                    mid += 2 * x - 1;
+                    swap(mid, s[!x]);
+                    mid = (l + r) >> 1;
+                }
+                */
+                int mid, l = 0, r = int(nb) - 1;
+                while (l <= r) {
+                    mid = (l + r) >> 1;
+                    if (b[mid] < u) {
+                        l = mid + 1;
+                    } else if (b[mid] > u) {
+                        r = mid - 1;
+                    } else {
+                        found = 1;
+                        break;
+                    }
+                }
+            }
+            out_offset[start_offset] = found;
+        }
+        __threadfence_block();
+        uint32_t tmp_sum = 0;
+        for (int i = (lid << shift); i < ((lid + 1) << shift); ++i)
+            tmp_sum += out_offset[i];//TODO: 这里有比较严重的bank conflict，考虑之后能不能用位运算加速？
+        tmp_prefix_sum[lid] = tmp_sum;
+        __threadfence_block();
+        for (int s = 1; s < THREADS_PER_WARP; s *= 2) {
+            uint32_t v = lid >= s ? tmp_prefix_sum[lid - s] : 0; //TODO: 考虑之后直接用寄存器shuffle而不是shared memory
+            __threadfence_block();
+            tmp_prefix_sum[lid] += v;
+            __threadfence_block();
+        }
+        if (lid == 0)
+            tmp_sum = 0;
+        else
+            tmp_sum = tmp_prefix_sum[lid - 1];
+        for (int i = (lid << shift); i < ((lid + 1) << shift); ++i)
+        {
+            tmp_sum += out_offset[i];//TODO: 这里有比较严重的bank conflict，考虑之后能不能用位运算加速？
+            out_offset[i] = tmp_sum;
+        }
+        __threadfence_block();
+
+        bool found;
+        if (lid == 0)
+            found = out_offset[0];
+        else
+            found = out_offset[lid] > out_offset[lid - 1];
+        if (found)
+            out[out_size + out_offset[lid] - 1] = a[num_done + lid];
+        for (int start_offset = lid + THREADS_PER_WARP; start_offset < concurrency; start_offset += THREADS_PER_WARP) //如果之后固定128，把这里展开会性能更好
+        {
+            if (out_offset[start_offset] > out_offset[start_offset - 1]) {
+                uint32_t offset = out_offset[start_offset] - 1;
+                out[out_size + offset] = a[num_done + start_offset];
+            }
+        }
+
+        if (lid == 0)
+            out_size += tmp_prefix_sum[THREADS_PER_WARP - 1];
+        num_done += concurrency;
+    }
+
+    __threadfence_block();
+    return out_size;
+}
+
 __device__ uint32_t do_intersection(uint32_t* out, const uint32_t* a, const uint32_t* b, uint32_t na, uint32_t nb)
 {
     __shared__ uint32_t block_out_offset[THREADS_PER_BLOCK];
@@ -582,7 +697,6 @@ __device__ unsigned long long IEP_3_layer_more_shared(const GPUSchedule* schedul
     uint32_t A_size = vertex_set[loop_set_prefix_ids[0]].get_size(), B_size = vertex_set[loop_set_prefix_ids[1]].get_size(), C_size = vertex_set[loop_set_prefix_ids[2]].get_size();
     
     if (C_size < MAX_SHARED_SET_LENGTH) // 即ABC都可以被放入shared memory
-    //if (false)
     {
         //TODO： 其实ABC都能被放入shared memory这个限制太严了，实际上只要AB能放入shared memory就可以，也就是一开始只将AB的元素都*2，然后任何集合在与C求交的时候再处理一下就可以（如果C作为a数组，就取出C元素后*2，如果C作为b数组，就把a数组中的元素取出后除2）
         A_ptr = warp_mem_start;
