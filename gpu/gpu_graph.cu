67ab90543ec05efd92a15548e072a2a2faeb70e0
zms1999 <zms_thu18@163.com>
2021-01-18 21:22:03 +0800

    fix answer redundancy bug on gpu

--- gpu/gpu_graph.cu
+++ gpu/gpu_graph.cu
@@ -762,6 +762,8 @@ void pattern_matching_init(Graph *g, const Schedule& schedule) {
     gpuErrchk( cudaDeviceSynchronize() );
     gpuErrchk( cudaMemcpyFromSymbol(&sum, dev_sum, sizeof(sum)) );
 
+    sum /= schedule.get_in_exclusion_optimize_redundancy();
+
     printf("count %llu\n", sum);
     tmpTime.print("Counting time cost");
     //之后需要加上cudaFree
