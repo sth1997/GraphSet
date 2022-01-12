be6cbb1c5a3688f627bcfc3030c31ab4f2f6b008
zms1999 <zms_thu18@163.com>
2022-01-13 00:14:22 +0800

    code for aws

--- tianhe/baseline_test.cpp
+++ tianhe/baseline_test.cpp
@@ -13,7 +13,7 @@
 
 void test_pattern(Graph* g, const Pattern &pattern, int performance_modeling_type, int restricts_type, bool use_in_exclusion_optimize = false) {
     printf("max intersection size %d\n", VertexSet::max_intersection_size);
-    int thread_num = 24;
+    int thread_num = 16;
     double t1,t2;
     
     bool is_pattern_valid;
@@ -26,8 +26,6 @@ void test_pattern(Graph* g, const Pattern &pattern, int performance_modeling_typ
 
     printf("ans %lld\n", ans);
     printf("time %.6lf\n", t2 - t1);
-//    printf("intersection %lld, %lld", g->intersection_times_low, g->intersection_times_high);
-//    printf("depth_cnt %lld %lld %lld\n", g->dep1_cnt, g->dep2_cnt, g->dep3_cnt);
     schedule.print_schedule();
     const auto& pairs = schedule.restrict_pair;
     printf("%d ",pairs.size());
@@ -45,11 +43,9 @@ int main(int argc,char *argv[]) {
     const std::string type = argv[1];
     const std::string path = argv[2];
     
-//    int size = atoi(argv[3]);
-//    char* adj_mat = argv[4];
+    int size = atoi(argv[3]);
+    char* adj_mat = argv[4];
 
-//    int test_type = atoi(argv[5]);
-    
     DataType my_type;
     
     GetDataType(my_type, type);
@@ -60,36 +56,12 @@ int main(int argc,char *argv[]) {
     }
 
     assert(D.load_data(g,my_type,path.c_str())==true); 
-    //assert(D.load_data(g,200)==true); 
 
     printf("Load data success!\n");
     fflush(stdout);
 
-    //char tmpbuf[100] = "011110101101110000110000100001010010";
-    Pattern p(PatternType::House);
-    //Pattern p(6, tmpbuf);
+    Pattern p(size, adj_mat);
     test_pattern(g, p, 1, 1, true);
-    //test_pattern(g, p, 0, 1, true);
-
-/*
-    Pattern p1(PatternType::sigmod2020_guo_q1);
-    test_pattern(g, p1, 1, 1, true);
-    
-    Pattern p2(PatternType::sigmod2020_guo_q2);
-    test_pattern(g, p2, 1, 1, true);
-    
-    Pattern p3(PatternType::sigmod2020_guo_q3);
-    test_pattern(g, p3, 1, 1, true);
-    
-    Pattern p4(PatternType::sigmod2020_guo_q4);
-    test_pattern(g, p4, 1, 1, true);
-    
-    Pattern p5(PatternType::sigmod2020_guo_q5);
-    test_pattern(g, p5, 1, 1, true);
-    
-    Pattern p6(PatternType::sigmod2020_guo_q6);
-    test_pattern(g, p6, 1, 1, true);
-*/
     delete g;
 }
 
