1a266beb3a8de682233d9801adbd387fa18d973a
haozixu <haozixu2001@qq.com>
2022-04-28 00:00:53 +0800

    add serial & parallel merge intersection

--- gpu/gpu_graph.cu
+++ gpu/gpu_graph.cu
@@ -20,6 +20,8 @@
 #include <sys/time.h>
 #include <chrono>
 
+#include "intersection.cuh"
+
 #define likely(x)   __builtin_expect(!!(x), 1)
 #define unlikely(x) __builtin_expect(!!(x), 0)
 constexpr int THREADS_PER_BLOCK = 256;
@@ -169,8 +171,7 @@ public:
 };
 
 // __device__ void intersection1(uint32_t *tmp, uint32_t *lbases, uint32_t *rbases, uint32_t ln, uint32_t rn, uint32_t* p_tmp_size);
-__device__ void intersection2(uint32_t *tmp, const uint32_t *lbases, const uint32_t *rbases, uint32_t ln, uint32_t rn, uint32_t* p_tmp_size);
-static __device__ uint32_t do_intersection(uint32_t*, const uint32_t*, const uint32_t*, uint32_t, uint32_t);
+__device__ void intersection2(uint32_t *out, uint32_t *tmp, const uint32_t *lbases, const uint32_t *rbases, uint32_t ln, uint32_t rn, uint32_t* p_tmp_size);
 class GPUVertexSet;
 __device__ int unordered_subtraction_size(const GPUVertexSet& set0, const GPUVertexSet& set1, int size_after_restrict);
 
@@ -215,7 +216,7 @@ public:
             data[i] = input_data[i];
         __threadfence_block();
     }
-    __device__ void build_vertex_set(const GPUSchedule* schedule, const GPUVertexSet* vertex_set, uint32_t* input_data, uint32_t input_size, int prefix_id)
+    __device__ void build_vertex_set(const GPUSchedule* schedule, const GPUVertexSet* vertex_set, GPUVertexSet& tmp_set, uint32_t* input_data, uint32_t input_size, int prefix_id)
     {
         int father_id = schedule->get_father_prefix_id(prefix_id);
         if (father_id == -1)
@@ -237,19 +238,19 @@ public:
                     this->size = vertex_set[father_id].get_size() - unordered_subtraction_size(vertex_set[father_id], *this, -1);
             }
             else {
-                intersection2(this->data, vertex_set[father_id].get_data_ptr(), input_data, vertex_set[father_id].get_size(), input_size, &this->size);
+                intersection2(this->data, tmp_set.get_data_ptr(),
+                    vertex_set[father_id].get_data_ptr(), input_data, vertex_set[father_id].get_size(), input_size, &this->size);
             }
         }
     }
 
-
-    __device__ void intersection_with(const GPUVertexSet& other)
-    {
-        uint32_t ret = do_intersection(data, data, other.get_data_ptr(), size, other.get_size());
-        if (threadIdx.x % THREADS_PER_WARP == 0)
-            size = ret;
-        __threadfence_block();
-    }
+    // __device__ void intersection_with(const GPUVertexSet& other)
+    // {
+    //     uint32_t ret = do_intersection(data, data, other.get_data_ptr(), size, other.get_size());
+    //     if (threadIdx.x % THREADS_PER_WARP == 0)
+    //         size = ret;
+    //     __threadfence_block();
+    // }
 
 private:
     uint32_t size;
@@ -259,126 +260,44 @@ private:
 __device__ unsigned long long dev_sum = 0;
 __device__ unsigned int dev_cur_edge = 0;
 
-/**
- * search-based intersection
- * 
- * returns the size of the intersection set
- * 
- * @note：a和b并不是地位相等的。如果要进行in-place操作，请把输入放在a而不是b。
- * @todo：shared memory缓存优化
- */
-__device__ uint32_t do_intersection(uint32_t* out, const uint32_t* a, const uint32_t* b, uint32_t na, uint32_t nb)
+
+__device__ uint32_t binary_search_intersection(uint32_t* out, uint32_t* tmp, const uint32_t* a, const uint32_t* b, uint32_t na, uint32_t nb)
 {
     __shared__ uint32_t block_out_offset[THREADS_PER_BLOCK];
     __shared__ uint32_t block_out_size[WARPS_PER_BLOCK];
 
     int wid = threadIdx.x / THREADS_PER_WARP; // warp id
-    int lid = threadIdx.x % THREADS_PER_WARP; // lane id
     uint32_t *out_offset = block_out_offset + wid * THREADS_PER_WARP;
     uint32_t &out_size = block_out_size[wid];
+    return warp_binary_search_intersection(out_offset, out_size, out, a, b, na, nb);
+}
 
-    if (lid == 0)
-        out_size = 0;
-
-    for(int num_done = 0; num_done < na; num_done += THREADS_PER_WARP) {
-        bool found = 0;
-        uint32_t u = 0;
-        if (num_done + lid < na) {
-            u = a[num_done + lid]; // u: an element in set a
-            /*
-            // 我这里这样写并没有变快，反而明显慢了
-            int x, s[3], &l = s[0], &r = s[1], &mid = s[2];
-            l = 0, r = int(nb) - 1, mid = (int(nb) - 1) >> 1;
-            while (l <= r && !found) {
-                uint32_t v = b[mid];
-                found = (v == u);
-                x = (v < u);
-                mid += 2 * x - 1;
-                swap(mid, s[!x]);
-                mid = (l + r) >> 1;
-            }
-            */
-            int mid, l = 0, r = int(nb) - 1;
-            while (l <= r) {
-                mid = (l + r) >> 1;
-                if (b[mid] < u) {
-                    l = mid + 1;
-                } else if (b[mid] > u) {
-                    r = mid - 1;
-                } else {
-                    found = 1;
-                    break;
-                }
-            }
-        }
-        out_offset[lid] = found;
-        __threadfence_block();
-
-        #pragma unroll
-        for (int s = 1; s < THREADS_PER_WARP; s *= 2) {
-            uint32_t v = lid >= s ? out_offset[lid - s] : 0;
-            __threadfence_block();
-            out_offset[lid] += v;
-            __threadfence_block();
-        }
-        
-        /*
-        // work-efficient parallel scan，但常数大，实测速度不行
-        #pragma unroll
-        for (int s = 1; s < THREADS_PER_WARP; s <<= 1) {
-            int i = (lid + 1) * s * 2 - 1;
-            if (i < THREADS_PER_WARP)
-                out_offset[i] += out_offset[i - s];
-            __threadfence_block();
-        }
-
-        #pragma unroll
-        for (int s = THREADS_PER_WARP / 4; s > 0; s >>= 1) {
-            int i = (lid + 1) * s * 2 - 1;
-            if ((i + s) < THREADS_PER_WARP)
-                out_offset[i + s] += out_offset[i];
-            __threadfence_block();
-        }
-        */
-        
-        if (found) {
-            uint32_t offset = out_offset[lid] - 1;
-            out[out_size + offset] = u;
-        }
-
-        if (lid == 0)
-            out_size += out_offset[THREADS_PER_WARP - 1];
-
-        /*
-        // 使用warp shuffle的scan，但实测速度更不行
-        uint32_t offset = found;
-        #pragma unroll
-        for (int i = 1; i < THREADS_PER_WARP; i *= 2) {
-            uint32_t t = __shfl_up_sync(0xffffffff, offset, i);
-            if (lid >= i)
-                offset += t;
-        }
-
-        if (found)
-            out[out_size + offset - 1] = u;
-        if (lid == THREADS_PER_WARP - 1) // 总和被warp中最后一个线程持有
-            out_size += offset;
-        */
-
-        //num_done += THREADS_PER_WARP;
-    }
-
-    __threadfence_block();
+__device__ uint32_t serial_merge_intersection(uint32_t* out, uint32_t* tmp, const uint32_t* a, const uint32_t* b, uint32_t na, uint32_t nb)
+{
+    uint32_t out_size = 0;
+    if (threadIdx.x % warpSize == 0)
+        out_size = merge(out, a, b, na, nb);
     return out_size;
 }
 
+__device__ uint32_t parallel_merge_intersection(uint32_t* out, uint32_t* tmp, const uint32_t* a, const uint32_t* b, uint32_t na, uint32_t nb)
+{
+    __shared__ uint32_t block_out_offset[THREADS_PER_BLOCK];
+    __shared__ uint32_t block_border[THREADS_PER_BLOCK];
+
+    int wid = threadIdx.x / warpSize;
+    auto out_offset = block_out_offset + wid * warpSize;
+    auto border = block_border + wid * warpSize;
+    return warp_parallel_merge_intersection(out_offset, border, out, tmp, a, b, na, nb);
+}
 
 /**
  * wrapper of search based intersection `do_intersection`
  * 
  * 注意：不能进行in-place操作。若想原地操作则应当把交换去掉。
  */
-__device__ void intersection2(uint32_t *tmp, const uint32_t *lbases, const uint32_t *rbases, uint32_t ln, uint32_t rn, uint32_t* p_tmp_size)
+__device__ void intersection2(uint32_t *out, uint32_t *tmp, const uint32_t *lbases, const uint32_t *rbases,
+    uint32_t ln, uint32_t rn, uint32_t* p_tmp_size)
 {
     // make sure ln <= rn
     if (ln > rn) {
@@ -389,7 +308,9 @@ __device__ void intersection2(uint32_t *tmp, const uint32_t *lbases, const uint3
      * @todo 考虑ln < rn <= 32时，每个线程在lbases里面找rbases的一个元素可能会更快
      */
 
-    uint32_t intersection_size = do_intersection(tmp, lbases, rbases, ln, rn);
+    // auto intersection_size = binary_search_intersection(out, tmp, lbases, rbases, ln, rn);
+    // auto intersection_size = serial_merge_intersection(out, tmp, lbases, rbases, ln, rn);
+    auto intersection_size = parallel_merge_intersection(out, tmp, lbases, rbases, ln, rn);
 
     if (threadIdx.x % THREADS_PER_WARP == 0)
         *p_tmp_size = intersection_size;
@@ -587,7 +508,7 @@ __device__ void GPU_pattern_matching_aggressive_func(const GPUSchedule* schedule
         bool is_zero = false;
         for (int prefix_id = schedule->get_last(depth); prefix_id != -1; prefix_id = schedule->get_next(prefix_id))
         {
-            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, &edge[l], r - l, prefix_id);
+            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, tmp_set, &edge[l], r - l, prefix_id);
             if (vertex_set[prefix_id].get_size() == 0) {
                 is_zero = true;
                 break;
@@ -736,7 +657,7 @@ __device__ void GPU_pattern_matching_func(const GPUSchedule* schedule, GPUVertex
         bool is_zero = false;
         for (int prefix_id = schedule->get_last(depth); prefix_id != -1; prefix_id = schedule->get_next(prefix_id))
         {
-            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, &edge[l], r - l, prefix_id);
+            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, tmp_set, &edge[l], r - l, prefix_id);
             if (vertex_set[prefix_id].get_size() == schedule->get_break_size(prefix_id)) {
                 is_zero = true;
                 break;
@@ -829,7 +750,7 @@ __global__ void gpu_pattern_matching(uint32_t edge_num, uint32_t buffer_size, ui
         bool is_zero = false;
         get_edge_index(v0, l, r);
         for (int prefix_id = schedule->get_last(0); prefix_id != -1; prefix_id = schedule->get_next(prefix_id))
-            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, &edge[l], r - l, prefix_id);
+            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, tmp_set, &edge[l], r - l, prefix_id);
 
         //目前只考虑pattern size>2的情况
         //start v1, depth = 1
@@ -839,7 +760,7 @@ __global__ void gpu_pattern_matching(uint32_t edge_num, uint32_t buffer_size, ui
         get_edge_index(v1, l, r);
         for (int prefix_id = schedule->get_last(1); prefix_id != -1; prefix_id = schedule->get_next(prefix_id))
         {
-            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, &edge[l], r - l, prefix_id);
+            vertex_set[prefix_id].build_vertex_set(schedule, vertex_set, tmp_set, &edge[l], r - l, prefix_id);
             if (vertex_set[prefix_id].get_size() == 0 && prefix_id < schedule->get_basic_prefix_num()) {
                 is_zero = true;
                 break;
