4df82a40015ade9c2dba839ace50a0ca18443858
PhoenixGS <thestarrydream@gmail.com>
2022-07-27 23:56:32 +0800

    add graph_mining
    添加graph_mining主程序

--- include/task_queue.h
+++ include/task_queue.h
@@ -1,14 +1,17 @@
 #include "graph_d.h"
 #include "embedding.h"
+#include <cstring>
 #include <vector>
 #include <algorithm>
 
-Class Task_Queue
+class Task_Queue
 {
 public:
     Task_Queue(Graph_D *G) //G表示分布式图储存
     {
         graph = G;
+        current_depth = 1;
+        memset(index, 0, sizeof(index));
         //
     }
     ~Task_Queue()
@@ -20,8 +23,8 @@ public:
     int current_machine[pattern_size];//每层当前在哪台机器
     int size[pattern_size];//记录每层目前大小
     std::vector<Embedding> q[pattern_size][machine_cnt]; //每一层，每一台机器，开一个vector储存
-    void insert(Embedding new_e);//加入一个embedding
-    Embedding new_task();//获取一个新任务,层数与embedding的size相同
-private:
+    int index[pattern_size][machine_cnt]; //每一层，每一台机器，已经算到了第几个
+    void insert(Embedding new_e, bool is_root = false);//加入一个embedding
+    Embedding* new_task();//获取一个新任务,层数与embedding的size相同
     Graph_D *graph;
 }
--- src/task_queue.cpp
+++ src/task_queue.cpp
@@ -1,37 +1,45 @@
 #include "../include/graph_d.h"
 #include "../include/embeddiing.h"
 #include "../include/task_queue.h"
+#include <omp.h>
 
 //Todo: 多线程
-void Task_Queue::insert(Embedding new_e, int depth)
+void Task_Queue::insert(Embedding new_e, bool is_root = false)
 {
     q[current_depth + 1][(*graph).get_block_index(new_e.last)].push_back(new_e);
     size[current_depth + 1]++;
-    if (size[current_depth + 1] >= Max_size)
+    if (is_root && size[current_depth + 1] >= Max_size)
     {
         current_depth++;
-        current_machine[current_depth] = (*graph).get_machine_id(); //Todo: 当前机器的编号
+        int N = (*graph).get_machine_cnt(); //Todo: 机器数量
+        int K = (*graph).get_machine_id(); //Todo: 当前机器的编号
+        current_machine[current_depth] = K;
+        for (int i = 0; i < N; i++)
+        {
+            index[current_depth][i] = 0;
+        }
     }
 }
 
 //Todo: 多线程
-Embedding Task_Queue::new_task()
+Embedding* Task_Queue::new_task()
 {
-    int N = (*graph).get_machine_cnt(); ////Todo: 机器数量
+    int N = (*graph).get_machine_cnt(); //Todo: 机器数量
     int K = (*graph).get_machine_id();
     while (current_depth >= 1)
     {
-        while (q[current_depth][current_machine[current_depth]].size() == 0 && (current_machine[current_depth] + 1) % N != K)
+        while (index[current_depth][current_machine[current_depth]] == (int)q[current_depth][current_machine[current_depth]].size() && (current_machine[current_depth] + 1) % N != K)
         {
             (current_machine[current_depth] += 1) %= N;
         }
-        if (q[current_depth][current_machine[current_depth]].size())
+        if (index[current_depth][current_machine[current_depth]] < (int)q[current_depth][current_machine[current_depth]].size())
         {
-            size[current_depth]--;
-            while (*q[current_depth][current_machine[current_depth]].back().get_state() != 1); //while直到状态变为Ready
-            Embedding e = (*q[current_depth][current_machine[current_depth]].back());
-            q[current_depth][current_machine[current_depth]].pop_back();
-            return e;
+            while (q[current_depth][current_machine[current_depth]][index[current_depth][current_machine[current_depth]]].get_state() != 1);
+            return &q[current_depth][current_machine[current_depth]][index[current_depth][current_machine[current_depth]]];
+        }
+        for (int i = 0; i < N; i++)
+        {
+            q[current_depth][i].clear();
         }
         current_depth--;
     }
