429d70ff6305e9d2989ba2a78edf9e4c57dc7926
PhoenixGS <thestarrydream@gmail.com>
2022-07-27 18:30:38 +0800

    fix some problem

--- include/task_queue.h
+++ include/task_queue.h
@@ -6,8 +6,10 @@
 Class Task_Queue
 {
 public:
-    Task_Queue()
+    Task_Queue(Graph_D *G, int N, int K) //G, N, K表示分布式图储存，机器总数量，当前机器编号(0~N-1)
     {
+        graph = G;
+        machine_cnt = N, machine_id = K;
         //
     }
     ~Task_Queue()
@@ -19,7 +21,9 @@ public:
     int current_machine[pattern_size];//每层当前在哪台机器
     int size[pattern_size];//记录每层目前大小
     std::vector<Embedding> q[pattern_size][machine_cnt]; //每一层，每一台机器，开一个vector储存
-    void insert(Embedding new_e,int depth);
-    //传一个数组，表示这层搜出的所有embeddings
+    void insert(Embedding new_e);//加入一个embedding
     Embedding new_task();//获取一个新任务,层数与embedding的size相同
+private:
+    int machine_cnt, machine_id;
+    Graph_D *graph;
 }
--- src/task_queue.cpp
+++ src/task_queue.cpp
@@ -1,19 +1,24 @@
+#include "../include/graph_d.h"
+#include "../include/embeddiing.h"
 #include "../include/task_queue.h"
 
+//Todo: 多线程
 void Task_Queue::insert(Embedding new_e, int depth)
 {
-    q[current_depth + 1][graph.get_block_index(new_e.last)].push_back(new_e);
+    q[current_depth + 1][(*graph).get_block_index(new_e.last)].push_back(new_e);
     size[current_depth + 1]++;
     if (size[current_depth + 1] >= Max_size)
     {
         current_depth++;
-        current_machine[current_depth] = graph.get_machine_id(); //Todo: 当前机器的编号
+        current_machine[current_depth] = (*graph).get_machine_id(); //Todo: 当前机器的编号
     }
 }
+
+//Todo: 多线程
 Embedding Task_Queue::new_task()
 {
-    int N = graph.get_machine_cnt(); //Todo: 机器数量
-    int K = graph.get_machine_id();
+    int N = (*graph).get_machine_cnt(); ////Todo: 机器数量
+    int K = (*graph).get_machine_id();
     while (current_depth >= 1)
     {
         while (q[current_depth][current_machine[current_depth]].size() == 0 && (current_machine[current_depth] + 1) % N != K)
