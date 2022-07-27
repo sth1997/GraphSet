17f9b40756602879201531c1eb366a2114cbdb3c
PhoenixGS <thestarrydream@gmail.com>
2022-07-27 17:23:01 +0800

    Add triangle_extend

--- src/kudu.cpp
+++ src/kudu.cpp
@@ -26,3 +26,41 @@ void fetch(std::vector<Embedding> &vec)
     }//Todo：一组同时进行通信
 }
 
+std::vector<Embedding> triangle_extend(Embedding *e)
+{
+    std::vector<Embedding> vec;
+    if ((*e).get_size() == 1)
+    {
+        Edges **list = (*e).get_list();
+        int cnt = list[0]->e_cnt;
+        for (int i = 0; i < cnt; i++)
+        {
+            int vet = list[0]->vet[i];
+            Embedding ep(e, vet[i]);
+            vec.push_back(ep);
+        }
+    }
+    else
+    {
+        Edges **list = (*e).get_list();
+        int cnt1 = list[0]->e_cnt;
+        int cnt2 = list[1]->e_cnt;
+        for (int i = 0; i < cnt1; i++)
+        {
+            bool flag = false;
+            for (int j = 0; j < cnt2; j++)
+            {
+                if ((list[0]->vet[i]) == (list[1]->vet[j]))
+                {
+                    flag = true;
+                }
+            }
+            if (flag)
+            {
+                Embedding ep(e, vet[i]);
+                vec.push_back(ep);
+            }
+        }
+    }
+    return vec;
+}
